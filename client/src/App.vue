401fd12e4f0e5d3442b816e9583f1ea5a81704ca
algolovanova@gmail.com <algolovanova@mail.ru>
2021-11-24 20:24:58 +0300

    work with DnD.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,8 +11,468 @@ export default {
 </script>
 
 <style>
-#app {
-
-  margin-top: 60px;
+#app {}
+.more-block {
+  align-items: flex-start;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 40px 0 -14px;
+  width: 100%;
 }
+
+@media screen and (min-width:920px) {
+  .more-block {
+    justify-content: space-between;
+  }
+}
+
+.more-block__heading {
+  display: block;
+  font-family: Open Sans,sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 26px;
+  margin-bottom: 12px;
+  width: 100%;
+}
+
+.more-block__item {
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: -2px -2px 5px rgba(255,255,255,.6),2px 2px 10px rgba(0,0,0,.2);
+  display: block;
+  margin-bottom: 14px;
+  max-width: 100%;
+  padding: 7px 9px 10px;
+  width: 100%;
+}
+
+@media screen and (min-width:920px) {
+  .more-block__item {
+    max-width: 332px;
+  }
+}
+
+.more-block__item-image {
+  border-radius: 3px;
+  margin-bottom: 7px;
+  width: 100%;
+}
+
+@media screen and (min-width:920px) {
+  .more-block__item-image {
+    height: 156px;
+  }
+}
+
+.more-block__item-title {
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  color: #000;
+  display: -webkit-box;
+  font-family: "PT Serif",serif;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 19px;
+  overflow: hidden;
+}
+
+.char-sheet {
+  display: flex;
+  flex-direction: column;
+  font-size: 13pt;
+  margin: 12px 12px 30px;
+  max-width: 700px;
+  min-height: 100vh;
+  padding: 0 0 1mm;
+  width: 100%;
+}
+
+@media print {
+  .char-sheet {
+    // eslint-disable-next-line
+    break-after: always;
+    break-inside: avoid;
+    margin: 0;
+    page-break-after: always;
+    page-break-before: always;
+  }
+}
+
+.char-sheet-info .char-sheet__name {
+  margin: 0 8pt 0 0;
+  min-width: 172pt;
+}
+
+.char-sheet-notes .char-sheet__undertext,.char-sheet-spells .char-sheet__undertext {
+  opacity: 0;
+  pointer-events: none;
+  position: relative;
+  top: -20pt;
+}
+
+@media print {
+  .char-sheet-notes .char-sheet__undertext,.char-sheet-spells .char-sheet__undertext {
+    opacity: 1;
+  }
+}
+
+.char-sheet-notes .char-sheet__undertext {
+  top: -10pt;
+}
+
+.char-sheet-spells .char-sheet__header {
+  height: auto;
+}
+
+.har-sheet__container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  min-height: 200vh;
+}
+
+.char-sheet__disclaimer {
+  margin-top: 32px;
+  max-width: 700px;
+  width: 100%;
+}
+
+.char-sheet__disclaimer-beta {
+  color: #4776e6;
+}
+
+@media print {
+  .char-sheet__disclaimer {
+    display: none;
+  }
+}
+
+.char-sheet__header {
+  align-items: center;
+  display: flex;
+  height: 70.5pt;
+  margin-bottom: 20pt;
+}
+
+.char-sheet__header.spells {
+  margin-bottom: 35pt;
+}
+
+.char-sheet__label {
+  font-family: Open Sans,sans-serif;
+  font-size: 6pt;
+  font-weight: 400;
+  margin-bottom: 0;
+  text-transform: uppercase;
+}
+
+.char-sheet__label.absolute {
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  transform: translateY(100%);
+}
+
+.char-sheet__label.centered {
+  display: block;
+  text-align: center;
+  width: 100%;
+}
+
+.char-sheet__label.min {
+  font-size: 5pt;
+}
+
+.char-sheet__name {
+  border-bottom: 1pt solid #000;
+  flex: 0 1;
+  font-size: 14pt;
+  margin: 0 9pt 0 0;
+  min-width: 169pt;
+  padding: 3pt 9pt;
+  position: relative;
+}
+
+.char-sheet__name .char-sheet__label {
+  margin-left: 9pt;
+  transform: translateY(calc(100% + 2pt));
+}
+
+.char-sheet__name .char-sheet__input {
+  border: 0;
+  padding: 0;
+  width: 100%;
+}
+
+.char-sheet__info {
+  border: 1pt solid #000;
+  border-radius: 2pt;
+  flex: 1 1;
+  padding: 4pt 10pt;
+}
+
+.char-sheet__info-section {
+  align-items: center;
+  border-bottom: 1pt solid #eaeaea;
+  display: flex;
+  margin-bottom: 15pt;
+}
+
+.char-sheet__info-box {
+  display: flex;
+  font-size: 10pt;
+  margin: 0 2pt;
+  position: relative;
+  width: 32%;
+}
+
+.char-sheet__info .char-sheet__input {
+  border: 0;
+  padding: 0;
+  width: 100%;
+}
+
+.char-sheet__info .char-sheet__info-box {
+  font-size: 9.3pt;
+}
+
+.char-sheet__body {
+  display: flex;
+  flex: 1 1;
+  margin: 0 -4pt;
+}
+
+.char-sheet__body-column {
+  display: flex;
+  flex-direction: column;
+  height: 668pt;
+  justify-content: flex-start;
+  margin: 0 4pt;
+  width: 33%;
+}
+
+.char-sheet__body-column.wide {
+  width: 66%;
+}
+
+.char-sheet__body .char-sheet__label {
+  font-weight: 700;
+}
+
+.char-sheet__stats-skills {
+  display: flex;
+  margin-bottom: 7pt;
+}
+
+.char-sheet__stats-skills .modifier-block:nth-of-type(2):before {
+  background: #4c453f;
+  border-left: 1pt solid #4c453f;
+  content: "";
+  display: block;
+  height: 14pt;
+  left: 4px;
+  position: absolute;
+  top: 20pt;
+}
+
+.char-sheet__stats {
+  background: #eaeaea;
+  border-radius: 30pt;
+  margin-right: 3pt;
+  padding: 24pt 2pt 8pt;
+}
+
+.char-sheet__stats_aime {
+  border-radius: 15pt;
+  padding: 15pt 2pt 0;
+}
+
+.char-sheet__skills {
+  flex: 1 1;
+}
+
+.char-sheet__attacks {
+  margin-bottom: 10pt;
+  max-height: 185.4pt;
+}
+
+.char-sheet__equipment {
+  display: block!important;
+}
+
+.char-sheet__equipment-wrapper {
+  display: flex;
+  flex: 1 1;
+  position: relative;
+}
+
+.char-sheet__equipment .char-sheet__label {
+  margin-top: 2pt;
+}
+
+.char-sheet__equipment .text-block__textarea {
+  height: 100%;
+  max-height: 219pt;
+}
+
+.char-sheet__devider {
+  height: 5pt;
+}
+
+.char-sheet__undertext {
+  // eslint-disable-next-line
+  break-after: always;
+  break-before: avoid;
+  color: #a4a4a4;
+  font-size: 6pt;
+  margin-bottom: 3pt;
+  text-align: center;
+}
+
+.char-sheet__undertext-link {
+  border-bottom: 1px solid #839ddc;
+  color: #839ddc;
+}
+
+@media print {
+  .char-sheet__undertext-link {
+    border-bottom: 0;
+    color: #a4a4a4;
+  }
+}
+
+.char-sheet__undertext-print {
+  display: none;
+}
+
+@media print {
+  .char-sheet__undertext-print {
+    display: inline-block;
+  }
+}
+
+.char-sheet__print {
+  align-items: center;
+  background: linear-gradient(90deg,#4776e6,#8e54e9 51%,#4776e6);
+  border-radius: 50%;
+  bottom: 80px;
+  box-shadow: 1px 1px 2px 1px rgba(0,0,0,.2);
+  cursor: pointer;
+  display: flex;
+  height: 45px;
+  justify-content: center;
+  position: fixed;
+  right: 84px;
+  width: 45px;
+  z-index: 999;
+}
+
+@media print {
+  .char-sheet__print {
+    display: none;
+  }
+}
+
+.char-sheet__print svg {
+  min-width: 24px;
+}
+
+@media screen and (max-width:799px) {
+  .char-sheet__container {
+    display: none;
+  }
+}
+
+.char-sheet__more {
+  padding: 12px;
+  width: 100%;
+}
+
+.char-sheet__more .more-block {
+  margin-top: 0;
+}
+
+.char-sheet__more .more-block__heading {
+  display: none;
+}
+
+@media screen and (min-width:800px) {
+  .char-sheet__more {
+    display: none;
+  }
+}
+
+.char-sheet input::-webkit-inner-spin-button,.char-sheet input::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.char-sheet input[type=number] {
+  -moz-appearance: textfield;
+}
+
+.char-sheet__enable-rolls {
+  align-items: center;
+  display: flex;
+  margin-bottom: 6px;
+}
+
+.char-sheet__enable-rolls-box {
+  margin-left: 8px;
+}
+
+.char-sheet__template-wrap {
+  align-items: flex-end;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  margin-bottom: 14px;
+}
+
+.character__wrapper {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.character__wrapper_disabled:after {
+  background: #fff;
+  bottom: 30px;
+  box-shadow: 1px 1px 2px 1px rgba(0,0,0,.2);
+  content: "Похоже, что редактор персонажа открыт сразу в нескольких вкладках.";
+  display: block;
+  left: 50%;
+  max-width: 780px;
+  padding: 30px;
+  position: fixed;
+  text-align: center;
+  transform: translateX(-50%);
+  white-space: pre-wrap;
+  width: 100%;
+  z-index: 9999;
+}
+
+.character__wrapper_disabled:before {
+  -webkit-backdrop-filter: blur(2px);
+  backdrop-filter: blur(2px);
+  background: rgba(255,255,255,.8);
+  content: "";
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 67px;
+  width: 100vw;
+  z-index: 9998;
+}
+
+@media print {
+  .no-print {
+    display: none!important;
+  }
+}
+input {
+  border-radius: 0;
+}
+
 </style>
--- client/src/components/DnD.vue
+++ client/src/components/DnD.vue
@@ -1,55 +1,79 @@
 <template>
-<div class="container px-4">
-  <div class="row gx-5">
-    <div class="col-6 col-md-4">
-     <div class="p-3 border bg-light">
-       <input type="text" class="form-control" placeholder="Character name"  >
-     </div>
-    </div>
-    <div class="col p-3 border bg-light">
-      <div class="row">
-        <div class="col ">
-          <div>
-            <input type="text" class="form-control" placeholder="Class">
-            <br>
-            <input type="text" class="form-control" placeholder="Experiens point" >
-          </div>
-        </div>
-        <div class="col">
-          <div >
-            <input type="text" class="form-control" placeholder="Race" >
-            <br>
-            <input type="text" class="form-control" placeholder="Alignment" >
-          </div>
-        </div>
-        <div class="col">
-          <div>
-            <input type="text" class="form-control" placeholder="Player name" >
-            <br>
-            <input type="text" class="form-control" placeholder="Blackgraund" >
-          </div>
+<div>
+  <div>
+    <div class="char-sheet__container">
+      <div class="character__wrapper">
+        <div class="char-sheet">
+
+          <header class="char-sheet__header">
+            <h1 class="char-sheet__name">
+              <input name="name" type="text" class="big char-sheet__input" autocomplete="new" value="">
+              <span class="char-sheet__label absolute">имя персонажа</span>
+            </h1>
+            <div class="char-sheet-info">
+              <div class="char-sheet__info-section">
+                <p class="char-sheet__info-box">
+                  <input name="charClass" type="text" class="small char-sheet__input" autocomplete="new" value="">
+                  <span class="char-sheet__label absolute">класс и уровень</span>
+                </p>
+                <p class="char-sheet__info-box">
+                  <input name="background" type="text" class="small char-sheet__input" list="list_of_background" autocomplete="new" value="">
+                  <datalist id="list_of_background">
+                    <option value="Blue"></option>
+                    <option value="Brown"></option>
+                    <option value="Orange"></option>
+                  </datalist>
+                  <span class="char-sheet__label absolute">предыстория</span>
+                </p>
+                <p class="char-sheet__info-box">
+                  <input name="playerName" type="text" class="small char-sheet__input" autocomplete="new" value="">
+                  <span class="char-sheet__label absolute">имя игрока</span>
+                </p>
+              </div>
+              <div class="char-sheet__info-section">
+                <p class="char-sheet__info-box">
+                  <input name="race" type="text" class="small char-sheet__input" list="list_of_race" autocomplete="new" value="">
+                  <datalist id="list_of_race">
+                    <option value="Blue"></option>
+                    <option value="Brown"></option>
+                    <option value="Orange"></option>
+                  </datalist>
+                  <span class="char-sheet__label absolute">раса</span>
+                </p>
+                <p class="char-sheet__info-box">
+                <input name="alignment" type="text" class="small char-sheet__input" list="list_of_worldview" autocomplete="new" value="">
+                  <datalist id="list_of_worldview">
+                    <option value="Blue"></option>
+                    <option value="Brown"></option>
+                    <option value="Orange"></option>
+                  </datalist>
+                  <span class="char-sheet__label absolute">мировоззрение</span>
+                </p><p class="char-sheet__info-box">
+                <input name="experience" type="number" class="small char-sheet__input"  list="list_of_experience" autocomplete="new" value="">
+                  <datalist id="list_of_experience">
+                      <option value="Blue"></option>
+                      <option value="Brown"></option>
+                      <option value="Orange"></option>
+                  </datalist>
+                <span class="char-sheet__label absolute">опыт</span>
+              </p>
+              </div>
+            </div>
+          </header>
+          <section class="char-sheet__body">
+            <div class="char-sheet__body-column">
+              <div class="char-sheet__body-column">
+                <
+              </div>
+            </div>
+          </section>
+
         </div>
       </div>
     </div>
   </div>
-    <div class="row gx-5">
-      <div class="col-6 col-md-2 p-3 border bg-light"">
-        <div>
-          <input type="text" class="form-control" placeholder="Strength" >
-          <br>
-          <input type="text" class="form-control" placeholder="Dexterity" >
-          <br>
-          <input type="text" class="form-control" placeholder="Constitution" >
-          <br>
-          <input type="text" class="form-control" placeholder="Intelligence" >
-          <br>
-          <input type="text" class="form-control" placeholder="Wisdom" >
-          <br>
-          <input type="text" class="form-control" placeholder="Charisma" >
-        </div>
-      </div>
-    </div>
 </div>
+
 </template>
 
 
@@ -83,5 +107,14 @@ export default {
 </script>
 
 <style scoped>
+.text-block-dop{
+  position: relative;
+  outline: none;
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
+  font-size: 9px;
+  line-height: 14px;
+  background: repeating-linear-gradient(rgb(255, 255, 255) 1px, rgb(255, 255, 255) 14px, rgb(218, 218, 218) 1px, rgb(218, 218, 218) 15px);
 
+}
 </style>
