c60c07b665f82776a0b7e7c4ec9e37d62e357839
algolovanova@gmail.com <algolovanova@mail.ru>
2021-11-24 15:00:14 +0300

    Start work with DnD.vue

--- client/src/components/DnD.vue
+++ client/src/components/DnD.vue
@@ -1,6 +1,38 @@
 <template>
-  <h1>{{ msg }}</h1>
-
+<div class="container px-4">
+  <div class="row gx-5">
+    <div class="col-6 col-md-4">
+     <div class="p-3 border bg-light">
+       <input type="text" class="form-control" placeholder="Character name"  >
+     </div>
+    </div>
+    <div class="col p-3 border bg-light">
+      <div class="row">
+        <div class="col ">
+          <div>
+            <input type="text" class="form-control" placeholder="Class">
+            <br>
+            <input type="text" class="form-control" placeholder="Experiens point" >
+          </div>
+        </div>
+        <div class="col">
+          <div >
+            <input type="text" class="form-control" placeholder="Race" >
+            <br>
+            <input type="text" class="form-control" placeholder="Alignment" >
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <input type="text" class="form-control" placeholder="Player name" >
+            <br>
+            <input type="text" class="form-control" placeholder="Blackgraund" >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
